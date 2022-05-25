3eef76f3f53a73fcf33c1ab4f75d624d182549ca
vazerick <PU7HCEV9ir6#!fJ$jc>
2022-05-25 15:14:16 -0300

    Início do formulário para adicionar eventos

--- app/calendario/adicionar.php
+++ app/calendario/adicionar.php
@@ -22,24 +22,132 @@ require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
 if (!securePage($_SERVER['PHP_SELF'])) {
     die();
 }
+
+
+function submit(){
+    var_dump($_POST);
+    $evento =  array();
+    $ErrorArrays = array();
+    if (empty($_POST["titulo_evento"])) {
+        $ErrorArrays[] = "Preencha o campo Título.";
+    } else {
+        $evento["titulo"] = $_POST["titulo_evento"];
+    }
+    if (empty($_POST["descricao"])) {
+        $evento["descricao"] = null;
+    } else {
+        $evento["descricao"] = $_POST["descricao"];
+    }
+    if (empty($_POST["data_inicio"])) {
+        $ErrorArrays[] = "Preencha o campo Data Início.";
+    } else {
+        $evento["data_inicio"] = $_POST["data_inicio"];
+    }
+    var_dump($evento);
+
+    if (count($ErrorArrays) == 0) {
+        echo "OK";
+    } else {
+        foreach ($ErrorArrays as $Errors) {
+            echo "<p style='color:red'><b>" . $Errors . "</p></b><br>";
+        }
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    submit();
+}
+
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang='pt-br'>
 <head>
+    <style>
+        /*input, textarea{*/
+        /*    width: 100%;*/
+        /*}*/
+    </style>
     <meta charset='utf-8' />
-
-    <?php
-    $db->query("SELECT * FROM calendario");
-    foreach ($db->results() as $record)
-        echo $record->title . "
-        ";
-    ?>
-
 </head>
 <body>
 
-<div id='calendar'></div>
+<div>
+    </br></br>
+    <h1>Adicionar novo evento</h1>
+    </br>
+    <form action="" method="post">
+        <div>
+            <label for="equipe">Equipe:</label>
+            <select id="equipe" name="equipe">
+                <option>
+                    Acadêmico
+                </option>
+            </select>
+            <label for="tipo">Tipo:</label>
+            <select id="tipo" name="tipo">
+                <option>
+                    Interno
+                </option>
+            </select>
+        </div>
+        <div>
+            <label for="titulo_evento">Título</label>
+            <input type="text" id="titulo_evento" name="titulo_evento" style="width: 100%">
+        </div>
+        <div>
+
+        </div>
+        <div>
+            <label for="descricao">Descrição</label>
+            <textarea id="descricao" name="descricao" style="width: 100%"></textarea>
+        </div>
+        <div>
+            <label for="allday">Dia inteiro</label>
+            <input type="checkbox" id="allday" name="allday">
+        </div>
+        <div>
+            <table style="width: auto">
+                <tr>
+                    <td>
+                        <label for="data_inicio">Data de início:</label>
+                    </td>
+                    <td>
+                        <input type="date" id="data_inicio" name="data_inicio">
+                    </td>
+                    <td style="width: 5%"></td>
+                    <td>
+                        <label for="hora_inicio">Horário de início:</label>
+                    </td>
+                    <td>
+                        <input type="time" id="hora_inicio" name="hora_inicio" value="00:01">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="data_fim">Data de fim:</label>
+                    </td>
+                    <td>
+                        <input type="date" id="data_fim" name="data_fim">
+                    </td>
+                    <td></td>
+                    <td>
+                        <label for="hora_fim">Horário de fim:</label>
+                    </td>
+                    <td>
+                        <input type="time" id="hora_fim" name="hora_fim" value="23:59">
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div style="float: right;">
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
+</div>
+
 
 </body>
 </html>
