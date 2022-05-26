57420eb6ef33ce45c0cbf91a248bc8787473810d
vazerick <PU7HCEV9ir6#!fJ$jc>
2022-05-26 16:57:46 -0300

    Início do formulário para visualizar eventos

--- app/calendario/visualizar.php
+++ app/calendario/visualizar.php
@@ -17,7 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-require_once 'users/init.php';
+require_once '../../users/init.php';
 require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
 if (!securePage($_SERVER['PHP_SELF'])) {
     die();
