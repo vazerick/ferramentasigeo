c043dfb48283e0f3ad50baf36a39fd2bdef58d96
vazerick <erickvaz.rs@gmail.com>
2022-05-27 16:06:15 -0300

    Cria o módulo de visualizar eventos

--- app/calendario/visualizar.php
+++ app/calendario/visualizar.php
@@ -22,11 +22,73 @@ require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
 if (!securePage($_SERVER['PHP_SELF'])) {
     die();
 }
+
+function escreve_data($data, $formato){
+    $date = strtotime($data);
+    return date($formato, $date);
+}
+
+$db->query("SELECT * FROM calendario WHERE id = '" . $_GET["id"] . "'");
+
+if((count($db->results()) == 0)){
+    echo "EVENTO NÃO LOCALIZADO";
+    die();
+}
+
+
+$evento = $db->results()[0];
+
+if(!(hasPerm($evento->group))){
+    echo "USUÁRIO NÃO AUTORIZADO PARA VER ESTE EVENTO";
+    die();
+}
+
 ?>
 
-<div class="row">
-	<div class="col-sm-12">
-
+<div class="container">
+    <div class="row align-items-start text-center">
+        <div style="margin-top: 1em; margin-bottom: 1em" class="col">
+            <button style="width: 75%" class="btn btn-primary">Calendário</button>
+        </div>
+        <div style="margin-top: 1em; margin-bottom: 1em" class="col">
+            <button style="width: 75%" class="btn btn-primary">Novo Evento</button>
+        </div>
+        <div style="margin-top: 1em; margin-bottom: 1em" class="col">
+            <button style="width: 75%" class="btn btn-primary">Editar</button>
+        </div>
+        <div style="margin-top: 1em; margin-bottom: 1em" class="col">
+            <button style="width: 75%" class="btn btn-warning">Deletar</button>
+        </div>
+    </div>
+	<div class="card-body">
+        <div class="card-header">
+        <h2><?php echo $evento->title; ?></h2>
+        </div>
+        <div class="card-text">
+            <div>
+        <?php echo "<strong>Tipo: </strong>" . "Interno" ?>
+            </div>
+        </div>
+        <div class="card-text">
+        <?php
+            $formato = "";
+            if($evento->allDay){
+                $formato = "d/m/Y";
+            } else {
+                $formato = "H:i - d/m/Y";
+            }
+            echo "<div ><strong>Início: </strong>";
+            echo escreve_data($evento->start, $formato) . "</div>";
+            echo "<div ><strong>Fim: </strong>";
+            echo escreve_data($evento->end, $formato) . "</div>";
+        ?>
+        </div>
+        <div class="card-body">
+            <div class='card-text'><strong>Descrição: </strong>
+                <div style="padding: 1em" class="card">
+                    <?php echo $evento->descr; ?>
+                </div>
+        </div>
 	</div>
 </div>
 
