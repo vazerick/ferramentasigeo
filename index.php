c552bbf50890f149f8ccc4b6d52e7650798ee9fd
vazerick <erickvaz.rs@gmail.com>
2022-05-31 10:37:24 -0300

    Inicia a construção da página inicial

--- index.php
+++ index.php
@@ -10,23 +10,39 @@ require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
 if(isset($user) && $user->isLoggedIn()){
 }
 ?>
-		<div class="jumbotron">
-            TESTE
-			<h1 align="center"><?=lang("JOIN_SUC");?> <?php echo $settings->site_name;?></h1>
-			<p align="center" class="text-muted"><?=lang("MAINT_OPEN")?></p>
+<!--<style>-->
+<!--    a.btn-primary {-->
+<!--        color: #fff;-->
+<!--    }-->
+<!--</style>-->
+		<div class="card-header">
+			<h1 align="center"><?php echo $settings->site_name;?></h1>
 			<p align="center">
 				<?php
 				if($user->isLoggedIn()){?>
-					<a class="btn btn-primary" href="users/account.php" role="button"><?=lang("ACCT_HOME");?> &raquo;</a>
+					<a class="btn btn-primary" href="users/account.php" role="button"> Perfil &raquo;</a>
+                    <a class="btn btn-warning" href="users/logout.php" role="button"> Sair &raquo;</a>
 				<?php }else{?>
 					<a class="btn btn-warning" href="users/login.php" role="button"><?=lang("SIGNIN_TEXT");?> &raquo;</a>
 					<a class="btn btn-info" href="users/join.php" role="button"><?=lang("SIGNUP_TEXT");?> &raquo;</a>
+<!--                    <a class="btn btn-warning" href="users/login.php" role="button">--><?//=lang("SIGNIN_TEXT");?><!-- &raquo;</a>-->
 				<?php }?>
 			</p>
 			<br>
-			<p align="center"><?=lang("MAINT_PLEASE");?></p>
-			<h4 align="center"><a href="https://userspice.com/getting-started/">https://userspice.com/getting-started/</a></h4>
 		</div>
+<?php
+if($user->isLoggedIn()){?>
+    <div class="container" style="margin-top: 2em;">
+        <div class="row">
+            <div class="col">
+                <a class="btn btn-primary" href="calendario.php" role="button">
+                    <i class="bi bi-calendar-event"></i>
+                    <p>Calendário</p>
+                </a>
+            </div>
+        </div>
+    </div>
+<?php }?>
 <?php  languageSwitcher();?>
 
 
